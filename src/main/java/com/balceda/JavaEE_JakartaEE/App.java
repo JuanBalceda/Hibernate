70414a3e439f966e222026b037612f9683767fc7
Juan Jesus Balceda Pomachagua <juanbalceda@gmail.com>
2018-07-06 11:55:56 +0200

    Database setup OK

---
+++ src/main/java/com/balceda/JavaEE_JakartaEE/App.java
@@ -0,0 +1,13 @@
+package com.balceda.JavaEE_JakartaEE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
